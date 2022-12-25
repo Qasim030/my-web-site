8ea7ef6e96ac00752375ce685a3f38fa7f219000
qasim <qasimmuniroo4@gmail.com>
2022-12-25 17:59:14 +0500

    last task has been done also

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,24 @@
 function checkPalindrome(string) {
-    const len = string.length;
+    var len = string.length;
     for (let i = 0; i < len / 2; i++) {
 
+        if(string.length <= 10){
+            console.log("You have Entered Less Character");
+        }
+       
         if (string[i] !== string[len - 1 - i]) {
             return 'It is not a palindrome';
         }
+
     }
     return 'It is a palindrome';
+
+    
 }
 
 const string = prompt('Enter a string: ');
 
 const value = checkPalindrome(string);
+
+
 console.log(value);
-
-
